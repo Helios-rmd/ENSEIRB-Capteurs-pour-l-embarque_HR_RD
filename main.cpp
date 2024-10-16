a7346233ede5d163e1d907eee8d042b392afee6f
DOAZAN Remi <Remi.Doazan@bordeaux-inp.fr>
2024-10-16 11:30:21 +0200

    Pilotage de la fréquence avec le bouton. Ajout de commentaires.

--- main.cpp
+++ main.cpp
@@ -12,16 +12,25 @@ using namespace std::chrono;
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
+#define BASE_FREQ         100ms
 InterruptIn button(BUTTON1);
 DigitalOut led(LED1);
 Timer t;
 Ticker flipper; 
-bool flag; 
+bool flag;
+int freq = 1; 
 
 void start_counting()
 {
     t.start();
     //led = true;
+    // int used to modify led blinking frequency
+    if(freq >= 6){
+        freq = 1;
+    }
+    else{
+        freq +=1;
+    }
 }
 
 void end_counting()
@@ -31,6 +40,7 @@ void end_counting()
     flag = true;
 }
 
+// Function called by ticker interruption to blink the LED 
 void flip_led()
 {
     led = !led;
@@ -41,12 +51,16 @@ int main()
     // interrupt when the button is pressed/released
     button.rise(&start_counting);  
     button.fall(&end_counting);
-    // ticker interruption. Call flip_led every 1s
-    flipper.attach(&flip_led, 500ms);
+    // ticker interruption. Call flip_led every BASE_FREQ ms. 
+    flipper.attach(&flip_led, BASE_FREQ);
 
     while (1) {          // wait around, interrupts will interrupt this!
         if(flag){
             printf("The button was pressed during %llu ms\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+            // Detach the flipper 
+            flipper.detach();
+            // Attach it with the new freq. 
+            flipper.attach(&flip_led, BASE_FREQ*freq); 
             flag = false; 
             t.reset();
         }
