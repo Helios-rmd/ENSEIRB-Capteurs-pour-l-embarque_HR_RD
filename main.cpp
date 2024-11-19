481ed38d59312b2ec60b196585fd6019673de0cc
DOAZAN Remi <Remi.Doazan@bordeaux-inp.fr>
2024-11-19 11:56:18 +0100

    Gestion blink de la led et affichage mesure capteur dans 2 threads differents

--- main.cpp
+++ main.cpp
@@ -12,18 +12,36 @@ using namespace std::chrono;
 
 I2C i2c(P1_I2C_SDA, P1_I2C_SCL);
 
+uint8_t addr7bit = 0x62;
+// Create sensor object
+Co2Sensor sensor = Co2Sensor(addr7bit, &i2c);
+DigitalOut led(LED1);
 
-int main()
+Thread thread_measure(osPriorityNormal, 1024, nullptr, nullptr); // 1024 = stack_size => mémoire allouée pour le thread
+Thread thread_blink(osPriorityNormal, 1024, nullptr, nullptr); // 1024 = stack_size => mémoire allouée pour le thread)
+
+void measure_routine()
 {
-    uint8_t addr7bit = 0x62;
-    // Create sensor object
-    Co2Sensor sensor = Co2Sensor(addr7bit, &i2c);
-    
-    float *result; 
     while(1)
     {
         sensor.make_measure();
         sensor.print_measure();
-        result = sensor.get_measure(); 
     }
+}
+
+void blink_routine()
+{
+    while(1)
+    {
+        led =!led;
+        ThisThread::sleep_for(1000ms);
+    }
+}
+
+int main()
+{
+   thread_measure.start(measure_routine);
+   thread_blink.start(blink_routine);
+   thread_measure.join();
+   thread_blink.join(); 
 }
